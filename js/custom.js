3f55add16db7349b1e842a8390c6ee9d30c8168f
shaydeecoder <shaydeecoder@gmail.com>
2020-07-16 09:34:51 +0100

    added popup to slider

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// Slider - Magnific Popup
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+
+$(function() {
+  $("#sliders").magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    // other options
+    gallery:{
+      enabled:true
+    }
+  });
+});
